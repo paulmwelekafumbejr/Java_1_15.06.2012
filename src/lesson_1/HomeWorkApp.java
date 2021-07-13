a62c716953f656def39ad9397cfb66268178a9bf
Paulgazza2008 <paulkafumbe@gmail.com>
2021-07-13 21:55:24 +0300

    HomeWorkApp

--- src/lesson_1/HomeWorkApp.java
+++ src/lesson_1/HomeWorkApp.java
@@ -24,7 +24,7 @@ public class HomeWorkApp {
         /**
          * Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением.
          *  Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”,
-         *   если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, 
+         *   если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”,
          *  если больше 100 (100 исключительно) - “Зеленый”;
          */
         compareNumbers();
@@ -36,6 +36,7 @@ public class HomeWorkApp {
          *
          */
     }
+
 
     private static void compareNumbers() {
         int a = 10;
@@ -62,7 +63,6 @@ public class HomeWorkApp {
     }
 
 
-
     private static void printThreeWords() {
         System.out.println(" _orange \n _banana \n _apple");
     }
@@ -79,11 +79,10 @@ public class HomeWorkApp {
         }
 
 
-        {
-
-
-
-        }
     }
 }
+
+
+
+
 
